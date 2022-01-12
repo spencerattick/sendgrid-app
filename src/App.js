1f5bbeb738229dd987ecd76503a23b0fae37114c
Spencer Attick <spencer.attick@gmail.com>
2022-01-12 08:33:37 -0800

    push initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,125 @@
-import logo from './logo.svg';
+import {useState, useEffect} from "react";
+import Papa from 'papaparse';
 import './App.css';
 
 function App() {
+  const [apiKeyValue, setApiKeyValue] = useState(null);
+  const [templateIdValue, setTemplateIdValue] = useState(null);
+  const [csvUpload, setCsvUpload] = useState(null);
+  const [sendGridEmailTemplate, setSendGridEmailTemplate] = useState(null);
+  const [sendApiRequest, setSendApiRequest] = useState(false);
+
+
+  function handleApiKeyValueChange(event) {
+     setApiKeyValue(event.currentTarget.value);
+   }
+
+  function handleTemplateIdValueChange(event) {
+    setTemplateIdValue(event.currentTarget.value);
+  }
+
+  function handleCsvUpload(event) {
+    event.preventDefault();
+    Papa.parse(event.target.files[0], {
+      complete: UpdateData,
+      header: true
+    });
+  }
+
+  function UpdateData(result) {
+    setCsvUpload(result.data);
+  }
+
+  function handlePreviewButtonClick(e) {
+    e.preventDefault();
+    setSendApiRequest(true);
+  }
+
+  function RenderSampleMessage() {
+    if(csvUpload && sendGridEmailTemplate) {
+      return (
+        <div>
+          <div dangerouslySetInnerHTML = {{__html: sendGridEmailTemplate}}/>
+        </div>
+      )
+    } else {
+      return (
+        <p></p>
+      )
+    }
+  }
+
+  function searchForVariables(templateText) {
+    let alteredSendGridEmailTemplate = templateText;
+    for (let key in csvUpload[0]) {
+      if (alteredSendGridEmailTemplate.includes(`{{${key}}}`)) {
+        alteredSendGridEmailTemplate = alteredSendGridEmailTemplate.replace(`{{${key}}}`, csvUpload[0][key]);
+      }
+    }
+    setSendGridEmailTemplate(alteredSendGridEmailTemplate);
+  }
+
+  useEffect(() => {
+    if (templateIdValue && apiKeyValue && csvUpload && sendGridEmailTemplate) {
+      searchForVariables(sendGridEmailTemplate);
+      RenderSampleMessage();
+    }
+  }, [sendGridEmailTemplate]);
+
+  useEffect(() => {
+    if (templateIdValue && apiKeyValue && csvUpload) {
+      let myHeaders = new Headers();
+      myHeaders.append("Authorization", `Bearer ${apiKeyValue}`);
+      myHeaders.append("Content-Type", "application/json");
+
+      var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+      };
+
+      fetch(`https://api.sendgrid.com/v3/templates/${templateIdValue}`, requestOptions)
+        .then(setSendApiRequest(false))
+        .then(response => response.text())
+        .then(result => setSendGridEmailTemplate(JSON.parse(result).versions[0].html_content))
+        .catch(error => console.log('error', error));
+    }
+  }, [sendApiRequest]);
+
+
+// https://stackoverflow.com/questions/44769051/how-to-upload-and-read-csv-files-in-react-js
+// https://daveceddia.com/useeffect-hook-examples/
+ // d-c03eb4b085e14884ad743f966aee96d4
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          SENDGRID APP
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form action="">
+          <input
+            type="password"
+            placeholder="SendGrid API Key"
+            onChange={handleApiKeyValueChange}/>
+          <input
+            type="text"
+            placeholder="Template ID"
+            onChange={handleTemplateIdValueChange}
+          />
+          <input
+           className="csv-input"
+           type="file"
+           name="file"
+           placeholder={null}
+           onChange={handleCsvUpload}
+           accept=".csv"
+         />
+       <button onClick={handlePreviewButtonClick}> Preview Message</button>
+       <button > Upload Emails</button>
+
+        </form>
+        <RenderSampleMessage></RenderSampleMessage>
       </header>
     </div>
   );
